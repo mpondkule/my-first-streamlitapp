aac5fd6d3f97096fee520353381c3385469aded1
mukund pondkule <mukund.pondkule@gmail.com>
2022-09-13 17:22:32 +0200

    some correction in app.py

--- src/app.py
+++ src/app.py
@@ -2,9 +2,9 @@
 import pandas as pd
 
 # matplotlib
-import matplotlib as mpl
+#import matplotlib as mpl
 import matplotlib.pyplot as plt
-import seaborn as sns
+#import seaborn as sns
 #plotly
 import plotly.express as px
 import plotly.graph_objects as go
